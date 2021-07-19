18fac91f7d57c8097bc60197c423b946b0173638
JosunLP <info@josunlp.de>
2021-07-19 12:17:04 +0200

    implementation

--- src/classes/Api.ts
+++ src/classes/Api.ts
@@ -12,31 +12,71 @@ export class Api {
     
     constructor(url: string) {
 
+        let result: ResultCollection;
+
         this.data = fetch("https://www.heise.de/security/rss/alert-news-atom.xml", Api.header)
             .then(response => response.text())
             .then(xml => {
                 const parser = new DOMParser();
                 const xmlDoc = parser.parseFromString(xml, "application/xml");
-                const result: ResultCollection = new ResultCollection([])
+                const rss: Element | null = Array.from(xmlDoc.getElementsByTagName("rss"))[0]
+                const atom: Element | null = Array.from(xmlDoc.getElementsByTagName("feed "))[0]
 
-                const feedItems = <HTMLCollectionOf<HTMLDivElement>>xmlDoc.getElementsByTagName("entry")
+                if (rss != null) {
+                    result = this.atomParser(xmlDoc)
+                }
 
-                Array.from(feedItems).forEach(entry => {
-                    const title = Array.from(entry.getElementsByTagName("title"))[0].innerHTML
-                    const id = Array.from(entry.getElementsByTagName("id"))[0].innerHTML
-                    const updated = Array.from(entry.getElementsByTagName("updated"))[0].innerHTML
-                    const link = <string>Array.from(entry.getElementsByTagName("link"))[0].getAttribute("href")
-                    const published = Array.from(entry.getElementsByTagName("published"))[0].innerHTML
-                    const summary = Array.from(entry.getElementsByTagName("summary"))[0].innerHTML
-                    const content = Array.from(entry.getElementsByTagName("content"))[0].innerHTML
-
-                    const item = new Result(title, id, updated, published, link, summary, content)                    
-
-                    result.collection.push(item)
-                })
+                if (atom != null) {
+                    result = this.rssParser(xmlDoc)
+                }
+               
 
                 return result
 
         })
+    }
+
+    private atomParser(xmlDoc: Document): ResultCollection {
+        const result: ResultCollection = new ResultCollection([])
+
+        const feedItems = <HTMLCollectionOf<HTMLDivElement>>xmlDoc.getElementsByTagName("entry")
+
+        Array.from(feedItems).forEach(entry => {
+            const title = Array.from(entry.getElementsByTagName("title"))[0].innerHTML
+            const id = Array.from(entry.getElementsByTagName("id"))[0].innerHTML
+            const updated = Array.from(entry.getElementsByTagName("updated"))[0].innerHTML
+            const link = <string>Array.from(entry.getElementsByTagName("link"))[0].getAttribute("href")
+            const published = Array.from(entry.getElementsByTagName("published"))[0].innerHTML
+            const summary = Array.from(entry.getElementsByTagName("summary"))[0].innerHTML
+            const content = Array.from(entry.getElementsByTagName("content"))[0].innerHTML
+
+            const item = new Result(title, id, updated, published, link, summary, content)
+
+            result.collection.push(item)
+        })
+
+        return result
+    }
+
+    private rssParser(xmlDoc: Document): ResultCollection {
+        const result: ResultCollection = new ResultCollection([])
+
+        const feedItems = <HTMLCollectionOf<HTMLDivElement>>xmlDoc.getElementsByTagName("item")
+
+        Array.from(feedItems).forEach(entry => {
+            const title = Array.from(entry.getElementsByTagName("title"))[0].innerHTML
+            const id = Array.from(entry.getElementsByTagName("guid"))[0].innerHTML
+            const updated = Array.from(entry.getElementsByTagName("updated"))[0].innerHTML
+            const link = <string>Array.from(entry.getElementsByTagName("link"))[0].innerHTML
+            const published = Array.from(entry.getElementsByTagName("pubDate"))[0].innerHTML
+            const summary = Array.from(entry.getElementsByTagName("description"))[0].innerHTML
+            const content = Array.from(entry.getElementsByTagName("content:encoded"))[0].innerHTML
+
+            const item = new Result(title, id, updated, published, link, summary, content)
+
+            result.collection.push(item)
+        })
+
+        return result
     }
 }
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,6 +8,11 @@
 
       <h2>Powered by <a href="https://heise.de">Heise.de</a></h2>
       <h4>Made available by <a href="https://JosunLP.de">JosunLP.de</a></h4>
+      <select id="feedChoice">
+        <option value="https://www.heise.de/security/rss/alert-news-atom.xml">Heise Atom</option>
+        <option value="https://www.heise.de/security/rss/alert-news.rdf">Heise RSS</option>
+        <option value="https://t3n.de/tag/software-infrastruktur/rss.xml">t3n RSS</option>
+      </select>
     </header>
     <div id="content"></div>
   </div>
@@ -28,18 +33,14 @@ export default class Main extends Vue {
 
 async function main() {
   const slider = new Slider();
+  const feed = <HTMLSelectElement>document.getElementById("feedChoice")
 
   slider.run(Config.itemName);
 
-  while (
-    (
-      await fetch(
-        "https://www.heise.de/security/rss/alert-news-atom.xml",
-        Api.header
-      )
-    ).ok
-  ) {
-    let api = new Api("https://www.heise.de/security/rss/alert-news-atom.xml");
+  while ((await fetch(feed.value, Api.header)).ok
+  ) 
+  {
+    let api = new Api(feed.value);
     let data = await api.data;
 
     Renderer.renderList(data, Config.containerName);
