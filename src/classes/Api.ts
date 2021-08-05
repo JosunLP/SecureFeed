2e29164a3cb1623bac2da67ed88f01987217a69b
Jonas Pfalzgraf <jolun.swtor@gmail.com>
2021-08-05 22:20:29 +0200

    tweaking the parser so that BSI Endpoints can be parsed

--- src/classes/Api.ts
+++ src/classes/Api.ts
@@ -18,7 +18,7 @@ export class Api {
             .then(response => response.text())
             .then(xml => {
                 const parser = new DOMParser();
-                const xmlDoc = parser.parseFromString(xml, "application/xml");
+                const xmlDoc = parser.parseFromString(xml, "application/xml")
                 const rss = xmlDoc.getElementsByTagName("item")[0]
                 const atom = xmlDoc.getElementsByTagName("entry")[0]
 
@@ -40,7 +40,12 @@ export class Api {
 
         Array.from(feedItems).forEach(entry => {
             const title = Array.from(entry.getElementsByTagName("title"))[0].innerHTML
-            const id = Array.from(entry.getElementsByTagName("id"))[0].innerHTML
+            let id;
+            try {
+                id = Array.from(entry.getElementsByTagName("id"))[0].innerHTML
+            } catch (error) {
+                id = "noIdGiven"
+            }            
             const updated = Array.from(entry.getElementsByTagName("updated"))[0].innerHTML
             const link = <string>Array.from(entry.getElementsByTagName("link"))[0].getAttribute("href")
             const published = Array.from(entry.getElementsByTagName("published"))[0].innerHTML
@@ -61,12 +66,22 @@ export class Api {
 
         Array.from(feedItems).forEach(entry => {
             const title = Array.from(entry.getElementsByTagName("title"))[0].innerHTML
-            const id = Array.from(entry.getElementsByTagName("guid"))[0].innerHTML
+            let id;
+            try {
+                id = Array.from(entry.getElementsByTagName("id"))[0].innerHTML
+            } catch (error) {
+                id = "noIdGiven"
+            }  
             const updated = Array.from(entry.getElementsByTagName("pubDate"))[0].innerHTML
             const link = Array.from(entry.getElementsByTagName("link"))[0].innerHTML
             const published = Array.from(entry.getElementsByTagName("pubDate"))[0].innerHTML
             const summary = Array.from(entry.getElementsByTagName("description"))[0].innerHTML
-            const content = Array.from(entry.getElementsByTagName("content:encoded"))[0].innerHTML
+            let content;
+            try {
+                content = Array.from(entry.getElementsByTagName("content:encoded"))[0].innerHTML
+            } catch (error) {
+                content = Array.from(entry.getElementsByTagName("description"))[0].innerHTML
+            }
 
             const item = new Result(title, id, updated, published, link, summary, content)
 
