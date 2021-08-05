6a1b09225ee055802d90819108ca2ecb7b0cad87
Jonas Pfalzgraf <jolun.swtor@gmail.com>
2021-08-05 22:04:17 +0200

    implementation of test proxy

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,30 +1,57 @@
 <template>
   <header>
-      <div id="brand">
-        <img src="@/assets/logo.png" />
-        <h1>SecureFeed</h1>
-      </div>
+    <div id="brand">
+      <img src="@/assets/logo.png" />
+      <h1>SecureFeed</h1>
+    </div>
 
-      <h4>Powered by <a href="https://heise.de" target="_blank">Heise.de</a> & <a href="https://www.bsi.bund.de/" target="_blank">BSI</a></h4>
-      <h5>Made available by <a href="https://JosunLP.de" target="_blank">JosunLP.de</a></h5>
-      <select id="feedChoice">
-        <option value="https://www.heise.de/security/rss/news.rdf">Heise Security News RSS</option>
-        <option value="https://www.heise.de/security/rss/alert-news.rdf">Heise Security Alert RSS</option>
-        <!-- Durch eine Entscheidung des BSI wird es vorläufig keine Feeds von ihrer Seite aus geben!
-        <option value="https://www.bsi.bund.de/SiteGlobals/Functions/RSSFeed/RSSNewsfeed/RSSNewsfeed.xml;jsessionid=C141D473EE32323BEAB05A41D119B51E.internet471"> BSI RSS-Newsfeed (Presse-, Kurzmitteilungen und Veranstaltungshinweise)</option>
-        <option value="https://www.bsi.bund.de/SiteGlobals/Functions/RSSFeed/RSSNewsfeed/RSSNewsfeed_WID.xml;jsessionid=C141D473EE32323BEAB05A41D119B51E.internet471"> BSI RSS-Newsfeed Kurzinformationen des CERT-Bund zu Sicherheitslücken und Schwachstellen in IT-Systemen</option>
-        <option value="https://www.bsi.bund.de/SiteGlobals/Functions/RSSFeed/RSSNewsfeed/RSSNewsfeed_BuergerCERT.xml;jsessionid=C141D473EE32323BEAB05A41D119B51E.internet471"> BSI RSS-Newsfeed Sicherheitshinweise des Buerger-CERT</option>
-        <option value="https://www.bsi.bund.de/SiteGlobals/Functions/RSSFeed/RSSNewsfeed/RSSNewsfeed_Produktzertifizierung.xml;jsessionid=C141D473EE32323BEAB05A41D119B51E.internet471"> BSI RSS-Newsfeed Produktzertifizierung</option>
-        -->
-        <option value="https://www.allianz-fuer-cybersicherheit.de/SiteGlobals/Functions/RSSFeed/RSSNewsfeed/ACS_RSSNewsfeed.xml"> BSI RSS-Newsfeed ACS Aktuelle Informationen</option>
-      </select>
-    </header>
+    <h4>
+      Powered by <a href="https://heise.de" target="_blank">Heise.de</a
+      > & <a href="https://www.bsi.bund.de/" target="_blank">BSI</a>
+    </h4>
+    <h5>
+      Made available by
+      <a href="https://JosunLP.de" target="_blank">JosunLP.de</a>
+    </h5>
+    <select id="feedChoice">
+      <option value="https://www.heise.de/security/rss/news.rdf"
+        >Heise Security News RSS</option
+      >
+      <option value="https://www.heise.de/security/rss/alert-news.rdf"
+        >Heise Security Alert RSS</option
+      >
+      <option
+        value="https://localhost/API.php?apiMode=feed&feedMode=rss&dataUrl=https://www.bsi.bund.de/SiteGlobals/Functions/RSSFeed/RSSNewsfeed/RSSNewsfeed.xml;jsessionid=C141D473EE32323BEAB05A41D119B51E.internet471"
+      >
+        BSI (Presse-, Kurzmitteilungen und Veranstaltungshinweise)</option
+      >
+      <option
+        value="https://localhost/API.php?apiMode=feed&feedMode=rss&dataUrl=https://www.bsi.bund.de/SiteGlobals/Functions/RSSFeed/RSSNewsfeed/RSSNewsfeed_WID.xml;jsessionid=C141D473EE32323BEAB05A41D119B51E.internet471"
+      >
+        BSI Kurzinformationen des CERT-Bund zu Sicherheitslücken und
+        Schwachstellen in IT-Systemen</option
+      >
+      <option
+        value="https://localhost/API.php?apiMode=feed&feedMode=rss&dataUrl=https://www.bsi.bund.de/SiteGlobals/Functions/RSSFeed/RSSNewsfeed/RSSNewsfeed_BuergerCERT.xml;jsessionid=C141D473EE32323BEAB05A41D119B51E.internet471"
+      >
+        BSI Sicherheitshinweise des Buerger-CERT</option
+      >
+      <option
+        value="https://localhost/API.php?apiMode=feed&feedMode=rss&dataUrl=https://www.bsi.bund.de/SiteGlobals/Functions/RSSFeed/RSSNewsfeed/RSSNewsfeed_Produktzertifizierung.xml;jsessionid=C141D473EE32323BEAB05A41D119B51E.internet471"
+      >
+        BSI Produktzertifizierung</option
+      >
+      <option
+        value="https://localhost/API.php?apiMode=feed&feedMode=rss&dataUrl=https://www.allianz-fuer-cybersicherheit.de/SiteGlobals/Functions/RSSFeed/RSSNewsfeed/ACS_RSSNewsfeed.xml"
+      >
+        Allianz für Cybersicherheit ACS Aktuelle Informationen</option
+      >
+    </select>
+  </header>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
@@ -34,7 +61,8 @@ header {
   height: 6rem;
 }
 
-h4, h5 {
+h4,
+h5 {
   padding-top: 0;
   padding-bottom: 0;
   margin-top: 0.5em;
@@ -56,7 +84,9 @@ h4, h5 {
   padding-right: 0.5em;
 }
 
-#feedChoice, #feedChoice:focus, #feedChoice:scope {
+#feedChoice,
+#feedChoice:focus,
+#feedChoice:scope {
   color: white;
   background-color: #006882;
   border-color: #006882;
@@ -66,5 +96,4 @@ h4, h5 {
   border-color: none;
   padding-left: 0;
 }
-
 </style>
