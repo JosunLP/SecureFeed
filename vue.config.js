0a575073c9b3da1b543caa1bcd7841931b0ed881
Jonas Pfalzgraf <j.pfalzgraf@ccvossel.de>
2021-10-12 14:50:54 +0200

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,10 @@
 // vue.config.js file to be place in the root of your repository
 
 module.exports = {
-    chainWebpack: config => {
-        config.module.rules.delete('eslint');
-    },
-
-    runtimeCompiler: true,
-
-    css: {
-      sourceMap: true
-    }
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/securefeed/'
+      : '/',
+  chainWebpack: config => {
+    config.module.rules.delete('eslint');
+  }
 }
