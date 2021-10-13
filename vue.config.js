417703ba54153da444492f1f78e506980edb19e9
Jonas Pfalzgraf <jolun.swtor@gmail.com>
2021-10-13 14:36:29 +0200

    readding vue config

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+// vue.config.js file to be place in the root of your repository
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/securefeed/'
+      : '/',
+  chainWebpack: config => {
+    config.module.rules.delete('eslint');
+  }
+}
